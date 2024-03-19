a6f46b61bb54465f291a0900abb674b11c161b0c
Theshikanavod <theshikanavod@gmail.com>
2024-03-19 23:45:28 +0530

    commit2

--- e_cimmerce_frontend/src/components/HeaderTop.js
+++ e_cimmerce_frontend/src/components/HeaderTop.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import {BsFacebook, BsLinkedin, BsTwitter,BsInstagram} from "react-icons/bs"
 
 const HeaderTop = () => {
   return (
-    <div>HeaderTop</div>
+    <div className='border-b border-gray-200 hidden '>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <BsFacebook />
+                    </div>
+                    <div>
+                        <BsTwitter/>
+                    </div>
+                    <div>
+                        <BsInstagram />
+                    </div>
+                    <div>
+                        <BsLinkedin />
+                    </div>
+
+                </div>
+
+
+
+                <div className='text-gray-500 text-[12px]'>
+                    <strong>FREE SHIPPING</strong>
+                </div>
+
+
+                <div>
+                    <select name='currancy' id='currncy'>
+                        <option value='USD $'>USD $</option>
+                        <option value='EUR $'>EUR $</option>
+                    </select>
+
+                    <select name='language' id='language'>
+                        <option value='English'>English</option>
+                        <option value='Freanch'>Freanch</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
